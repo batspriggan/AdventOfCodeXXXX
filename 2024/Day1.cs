40ff3c14900cc82521aa835ffe436f46e35702f0
ilBat77 <alessandro.battini@gmail.com>
2024-12-01 11:20:01 +0100

    20242 day1

---
+++ 2024/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+using System.Reflection.Emit;
+using AdventOfCodeCommon;
+namespace AdventOfCode;
+
+
+internal class Day1 : AdventOfCodeDay
+{
+    public override int DayNumber => 1;
+
+    public override string Calculate_1()
+    {
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+        foreach (var line in ReadDayFile())
+        {
+            var splitted = line.Split(' ').ToList();
+            list1.Add(int.Parse(splitted.First()));
+            list2.Add(int.Parse(splitted.Last()));
+        }
+        list1 = list1.OrderBy(x => x).ToList();
+        list2 = list2.OrderBy(x => x).ToList();
+        int totalDistance = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            totalDistance += Math.Abs(list1[i] - list2[i]);
+        }
+        return totalDistance.ToString();
+
+    }
+    public override string Calculate_2()
+    {
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+        foreach (var line in ReadDayFile())
+        {
+            var splitted = line.Split(' ').ToList();
+            list1.Add(int.Parse(splitted.First()));
+            list2.Add(int.Parse(splitted.Last()));
+        }
+        int total = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            var similarity = list2.Count(x => x == list1[i]);
+            total += list1[i] * similarity;
+        }
+        return total.ToString();
+    }
+}
---
+++ 2024/Program.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCodeCommon;
+
+var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => typeof(AdventOfCodeDay).IsAssignableFrom(p) && p.Name != typeof(AdventOfCodeDay).Name);
+
+
+List<AdventOfCodeDay> Instances = new List<AdventOfCodeDay>();
+foreach (var type in types)
+{
+    var day = (AdventOfCodeDay)Activator.CreateInstance(type);
+    day.Year = 2024;
+    Instances.Add(day);
+
+}
+
+Instances.OrderBy(x => x.DayNumber).ToList().ForEach(x => Console.WriteLine(x.DayResults));
